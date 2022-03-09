7b4e8d33ff79b4d7b74a8a0058f827b50c9be431
Martin-Salomon <martinsalomon11@gmail.com>
2022-03-09 15:46:47 -0300

    mi primer commit

---
+++ Json.js
@@ -0,0 +1,20 @@
+/* let amigos = ['Coco','Pablito', 'Sus', 'Franco'];
+
+let amigosJson = JSON.stringify(amigos);
+
+let amigosOriginal = JSON.parse(amigosJson);
+
+console.log(amigosOriginal);
+ */
+
+let persona = {
+    nombre : 'Carli',
+    edad : 26,
+    domicilio : 'Calle Falsa 123',
+}
+
+let personaJson = JSON.stringify(persona);
+
+let personaOriginal = JSON.parse(personaJson)
+
+console.log(personaJson);
---
+++ funcionesDeTareas.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+
+let archivoTareas = {
+    leerArchivo: function(){
+        let tareasJson = fs.readFileSync("tareas.json", "utf-8");
+        return JSON.parse(tareasJson);
+    },
+    escribirJson: function(tareas){
+        tareasJson = JSON.stringify(tareas, null, " ")
+        fs.writeFileSync("tareas.json", tareasJson);
+    },
+    guardarTarea: function(tarea){
+        // leer el array de tareas
+        let tareas = this.leerArchivo();
+        // agregamos la tarea al final del array
+        tareas.push(tarea);
+        // guardamos en el json
+        this.escribirJson(tareas);
+    },
+    filtrarPorEstado: function(estado){
+        // leer el array de tareas
+        let tareas = this.leerArchivo();
+        // utilizamos un filter para crear el nuevo array ya filtrado
+        let tareasFiltradas = tareas.filter((tarea) => {
+            return tarea.estado === estado;
+        });
+        // devolvemos el array ya filtrado
+        return tareasFiltradas;
+    }
+}
+
+module.exports = archivoTareas;
